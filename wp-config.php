a3959d389bde3c895449d9670fcc3148691a8e97
porkunanongithub <porkunanon@gmail.com>
2018-08-08 14:20:01 +0700

    add

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'sql12250053');
+define('DB_NAME', 'wp_ecommerce');
 
 /** MySQL database username */
-define('DB_USER', 'sql12250053');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'FTYNM2Kzyp');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
-define('DB_HOST', 'sql12.freemysqlhosting.net');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8mb4');
